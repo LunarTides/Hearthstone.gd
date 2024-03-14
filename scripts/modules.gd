91f90daf730a0e1d9c151c758b00f2763bf7e7a3
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-03-14 13:11:19 +0100

    Fix queue in module

--- scripts/modules.gd
+++ scripts/modules.gd
@@ -14,6 +14,7 @@ var _running: bool = false
 var _result: bool = true
 var _modules_responded: int = 0
 
+var _queue: Array
 var _modules_for_signal: Dictionary
 #endregion
 
@@ -57,14 +58,28 @@ func wait_for_response(_signal: Signal) -> Dictionary:
 ## [/codeblock]
 func wait_in_queue(_signal: Signal) -> void:
 	if not _running:
+		# Don't add to queue if the module system is idle.
 		return
 	
-	print_verbose("[Modules] `%s` waiting in queue..." % _signal.get_name())
-	await wait_for_response(_signal)
-	print_verbose("[Modules] Queue over.")
+	# Add to queue.
+	var id: int = ResourceUID.create_id()
+	_queue.append(id)
+	
+	print_verbose("[Modules] `%d` waiting in queue..." % id)
+	
+	while true:
+		await wait_for_response(_signal)
+		
+		if _queue[0] == id:
+			break
+	
+	print_verbose("[Modules] Queue over for `%d`." % id)
 	
 	# Same rationale as in `_register_hook`.
 	await get_tree().process_frame
+	
+	# Remove from queue.
+	_queue.pop_front()
 #endregion
 
 
