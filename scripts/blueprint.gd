550c7af5ac1c7db1ebc87d65f360f7dca2b8e568
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-03-17 20:21:34 +0100

    Fix bugs and split rarity mesh into the rarity module

--- scripts/blueprint.gd
+++ scripts/blueprint.gd
@@ -15,7 +15,7 @@ enum {
 
 #region Exported Variables
 #region Common
-# TODO: Remove in Godot 4.3 or maybe 4.2.2
+# TODO: Remove in Godot 4.3 or 4.2.2
 ## This variable is to work around a bug. Please ignore.[br]
 ## See [url]https://github.com/godotengine/godot/pull/88318[/url]
 @export var ignore: int
